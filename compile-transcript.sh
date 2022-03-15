8ac67d709db2adbcfec7628cb8baaf7349fc1865
George Chang <59218296+gchang12@users.noreply.github.com>
2022-03-15 09:59:34 -1000

    Add files via upload

--- compile-transcript.sh
+++ compile-transcript.sh
@@ -5,17 +5,9 @@ rm -fr filenames links content-creators.txt
 python3 ama_compiler.py
 # This user's comment had to be manually edited
 cp ../notes/Inazuma-sensei.txt "./Daron Nefcy/Inazuma-sensei.txt"
-# Sort directories into the order as it appears on the source website
-#touch "Daron Nefcy"
-touch "Adam McArthur"
-touch "Dominic Bisignano"
-touch "Aaron Hammersley"
 # Create list of directories to loop over during typesetting
-OIFS=$IFS
-IFS=$'\n'
-for i in $(ls -d --sort=time -r */); do echo ${i/\//} >> content-creators.txt; done
+for i in {"Daron Nefcy","Adam McArthur","Dominic Bisignano","Aaron Hammersley"}; do echo ${i/\//} >> content-creators.txt; done
 mv -f content-creators.txt ..
-IFS=$OIFS
 # Create ordered list of comments to loop over during typesetting
 if [ ! -e "filenames" ]; then mkdir "filenames"; fi;
 for i in ./*/; do ls --sort=time -r "$i" > "filenames/${i:2: -1}.txt"; done
