ea0d12dfc3c0010d90d33c1379cf2a1b6412baa0
George Chang <59218296+gchang12@users.noreply.github.com>
2022-03-15 10:58:49 -1000

    Update compile-transcript.sh

--- compile-transcript.sh
+++ compile-transcript.sh
@@ -2,15 +2,14 @@
 cd src
 python3 ama_compiler.py
 # Create ordered list of comments to loop over during typesetting
-if [ ! -e "filenames" ]; then (mkdir "filenames"; for i in ./*/; do (ls --sort=time -r "$i" > "filenames/${i:2: -1}.txt"); done; rm filenames/filenames.txt); fi
-if [ ! -e ../filenames/ ]; then cp filenames ..; fi
+if [ ! -e "../filenames" ]; then (mkdir "filenames"; for i in ./*/; do (ls --sort=time -r "$i" > "filenames/${i:2: -1}.txt"); done; rm filenames/filenames.txt; mv "filenames" ..); fi
 # This user's comment had to be manually edited
-cp ../notes/Inazuma-sensei.txt "./Daron Nefcy/Inazuma-sensei.txt"; fi
+cp ../notes/Inazuma-sensei.txt "./Daron Nefcy/Inazuma-sensei.txt"
 # Getting hyperlinks for each user
 if [ ! -e "../links/" ]; then (python3 link_fetcher.py; mv links ..); fi
 cd ..
 # Create list of directories to loop over during typesetting
-for i in {"Daron Nefcy","Adam McArthur","Dominic Bisignano","Aaron Hammersley"}; do echo ${i/\//} >> content-creators.txt; done
+if [ ! -e content-creators.txt ]; then (for i in {"Daron Nefcy","Adam McArthur","Dominic Bisignano","Aaron Hammersley"}; do echo ${i/\//} >> content-creators.txt; done); fi
 # Convert input files into TeX-friendly format
 python3 parser.py
 # Create emoticon files for use during typesetting
